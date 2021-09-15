9f41e70cf145ef11843f511b36a25a9b62d7652c
ThaiGang <85010385+ThaiGang@users.noreply.github.com>
2021-09-16 03:25:21 +0900

    배열2

--- array_study.java
+++ array_study.java
@@ -1,16 +1,20 @@
-package ex14.array;
+package ex15.Marray_2;
 import java.util.Scanner;
 public class array_study {
 
 	public static void main(String[] args) {
-				int total,select;
+				int total = 0,select;
 				float avg;
 				
 				Scanner scan = new Scanner(System.in);
 				
-				int[] kors = new int[3];
-				for (int i = 0; i>3;i++) {
-					kors[i] = 0;
+				//int[] kors = new int[3*3];
+				int[][] kors = new int[3][3];
+				for (int j = 0; j>3*3;j++) {
+					for (int i = 0; i>3*3;i++) {
+						//kors[i] = 0;
+						kors[j][i] = 0;
+					}
 				}
 				select=0;
 			종료:
@@ -31,29 +35,34 @@ public class array_study {
 					System.out.println("┌─────────────┐");
 					System.out.println("│   성적 입력    │");
 					System.out.println("└─────────────┘");
-					for(int i = 0;i<3;i++) {
-						do {
-							System.out.printf("국어%d : ",i+1);
-							kors[i] = scan.nextInt();
-							if(kors[i]<0 || kors[i]>100){
-								 System.out.printf("성적범위(0~100)를 벗어났습니다\n");
-							}
-						}while(kors[i]<0 || kors[i]>100);
-					}
+					for(int j = 0;j<3;j++) {
+						for(int i = 0;i<3;i++) {
+							do {
+								System.out.printf("%d학년 국어%d : ",j+1,i+1);
+								kors[j][i]= scan.nextInt();
+								if(kors[j][i]<0 || kors[j][i]>100){
+									 System.out.printf("성적범위(0~100)를 벗어났습니다\n");
+								}
+							}while(kors[j][i]<0 || kors[j][i]>100);
+						}}
 
 					break;
 				case 2: 
 						// ---------- 성적 출력 부분 ----------
-					total= kors[0]+kors[1]+kors[2];
-					avg = (float)(total / 3);
-						System.out.println("┌─────────────┐");
-						System.out.println("│   성적 출력    │");
-						System.out.println("└─────────────┘");
-						 for(int i = 0;i>3;i++) {
-							 System.out.printf("국어%d : %d\n",i+1,kors[i]);
-						 }
-						System.out.printf("총점 : %d\n",total);
-						System.out.printf("평균 : %3.2f\n",avg);
+							System.out.println("┌─────────────┐");
+							System.out.println("│   성적 출력    │");
+							System.out.println("└─────────────┘");
+						for(int j = 0;j<3;j++) {
+							System.out.printf("<--- %d학년 국어 성적 --->\n",j+1);
+							 for(int i = 0;i<3;i++) {
+								 System.out.printf("국어%d : %d\n",i+1,kors[j][i]);
+								 total = total + kors[j][i];
+							 }
+								avg = (float)(total / 3);
+								System.out.printf("총점 : %d\n",total);
+								System.out.printf("평균 : %3.2f\n",avg);
+								total = 0;
+							}
 						break;
 				case 3:
 						break 종료;
